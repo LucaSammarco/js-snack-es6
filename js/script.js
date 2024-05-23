b970421248f1806e95218de602b909ff8e19750d
Il tuo nome <la_tua_email@example.com>
2024-05-23 18:52:04 +0200

    working on generating the list of uppercase names

--- js/script.js
+++ js/script.js
@@ -16,10 +16,29 @@
 
 
 
+
+
+
+//* Snack 1
+
+// const tavoloVip = [ 'Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni',  'George Clooney', 'Amal Clooney', 'Fedez', 'Amadeus', 'Fiorello']
+
+
+// nuovaLista = tavoloVip.map((x, index) => {
+//     return ({tableName: "tavoloVip", guestName: x, place:index+1 })
+// });
+
+
+// console.log(nuovaLista)
+
+
 // SNACK 2
 // Abbiamo un elenco degli studenti di una facoltà, identificati da id, Nome e somma totale dei loro voti di esame...
+
 // 1. Per preparare l'aula di un nuovo corso, dobbiamo stampare le targhe col nome degli studenti: creare una lista contenente il loro nome tutto in maiuscolo, ad esempio (Marco della Rovere => MARCO DELLA ROVERE);
+
 // 2. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70
+
 // 3. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70 e id superiore a 120
 // Buon pomeriggio e buon lavoro !!! :faccia_nerd:
 
@@ -34,14 +53,25 @@
 // 120 Francesca da Polenta    84
 
 
-//* Snack 1
+//! Creazione lista studenti
+const studenti = [
+    { id: 213, name: 'Giuseppina della Rovere', grades: 78 },
+    { id: 110, name: 'Paola Cortellessa', grades: 96 },
+    { id: 250, name: 'Andrea Mantegna', grades: 48 },
+    { id: 145, name: 'Gaia Borromini', grades: 74 },
+    { id: 196, name: 'Luigi Grimaldello', grades: 68 },
+    { id: 102, name: 'Piero della Francesca', grades: 50 },
+    { id: 120, name: 'Francesca da Polenta', grades: 84 }
+];
 
-const tavoloVip = [ 'Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni',  'George Clooney', 'Amal Clooney', 'Fedez', 'Amadeus', 'Fiorello']
 
 
-nuovaLista = tavoloVip.map((x, index) => {
-    return ({tableName: "tavoloVip", guestName: x, place:index+1 })
-});
+const targhe = studenti.map((y) => y.nome{
+    
+return ({name: y })
 
+ });
 
-console.log(nuovaLista)
+console.log(targhe)
+
+// console.log(studenti);
