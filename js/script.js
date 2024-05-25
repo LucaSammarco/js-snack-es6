4035051d199f304a4f4121a561f0f94c1911cfac
Il tuo nome <la_tua_email@example.com>
2024-05-25 18:17:22 +0200

    trying to find min weight

--- js/script.js
+++ js/script.js
@@ -1,86 +1,86 @@
-// SNACK 1
-// Dwayne Johnson ci ha chiesto di creare i segnaposto per il tavolo degli invitati alla sua mega festa vip.
-// Ci ha lasciato il nome del tavolo ("Tavolo Vip") e la lista degli invitati in ordine di posto:
+// // SNACK 1
+// // Dwayne Johnson ci ha chiesto di creare i segnaposto per il tavolo degli invitati alla sua mega festa vip.
+// // Ci ha lasciato il nome del tavolo ("Tavolo Vip") e la lista degli invitati in ordine di posto:
 
-// [ 'Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni',  'George Clooney', 'Amal Clooney', 'Fedez', 'Amadeus', 'Fiorello']
-
-
-// Ma  la tipografia per stampare il tutto vuole che le mandiamo una lista di ospiti in un formato specifico, per cui dobbiamo fare in modo che ogni ospite sia un oggetto letterale javascript che ha come proprietà:
+// // [ 'Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni',  'George Clooney', 'Amal Clooney', 'Fedez', 'Amadeus', 'Fiorello']
 
 
-// nome del tavolo (tableName),
-// nome dell'ospite (guestName),
-// posto occupato (place),
-
-// Generiamo e stampiamo in console la lista per i segnaposto.
+// // Ma  la tipografia per stampare il tutto vuole che le mandiamo una lista di ospiti in un formato specifico, per cui dobbiamo fare in modo che ogni ospite sia un oggetto letterale javascript che ha come proprietà:
 
 
+// // nome del tavolo (tableName),
+// // nome dell'ospite (guestName),
+// // posto occupato (place),
+
+// // Generiamo e stampiamo in console la lista per i segnaposto.
 
 
 
 
-//* Snack 1
-
-const tavoloVip = [ 'Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni',  'George Clooney', 'Amal Clooney', 'Fedez', 'Amadeus', 'Fiorello']
 
 
-nuovaLista = tavoloVip.map((x, index) => {
-    return ({tableName: "tavoloVip", guestName: x, place:index+1 })
-});
+// //* Snack 1
+
+// const tavoloVip = [ 'Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni',  'George Clooney', 'Amal Clooney', 'Fedez', 'Amadeus', 'Fiorello']
 
 
-console.log(nuovaLista)
+// nuovaLista = tavoloVip.map((x, index) => {
+//     return ({tableName: "tavoloVip", guestName: x, place:index+1 })
+// });
 
 
-// SNACK 2
-// Abbiamo un elenco degli studenti di una facoltà, identificati da id, Nome e somma totale dei loro voti di esame...
-
-// 1. Per preparare l'aula di un nuovo corso, dobbiamo stampare le targhe col nome degli studenti: creare una lista contenente il loro nome tutto in maiuscolo, ad esempio (Marco della Rovere => MARCO DELLA ROVERE);
-
-// 2. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70
-
-// 3. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70 e id superiore a 120
-// Buon pomeriggio e buon lavoro !!! :faccia_nerd:
+// console.log(nuovaLista)
 
 
-// Id  Name                Grades
-// 213 Giuseppina della Rovere 78
-// 110 Paola Cortellessa       96
-// 250 Andrea Mantegna         48
-// 145 Gaia Borromini          74
-// 196 Luigi Grimaldello       68
-// 102 Piero della Francesca   50
-// 120 Francesca da Polenta    84
+// // SNACK 2
+// // Abbiamo un elenco degli studenti di una facoltà, identificati da id, Nome e somma totale dei loro voti di esame...
+
+// // 1. Per preparare l'aula di un nuovo corso, dobbiamo stampare le targhe col nome degli studenti: creare una lista contenente il loro nome tutto in maiuscolo, ad esempio (Marco della Rovere => MARCO DELLA ROVERE);
+
+// // 2. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70
+
+// // 3. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70 e id superiore a 120
+// // Buon pomeriggio e buon lavoro !!! :faccia_nerd:
 
 
-//! Creazione lista studenti
-const studenti = [
-    { id: 213, name: 'Giuseppina della Rovere', grades: 78 },
-    { id: 110, name: 'Paola Cortellessa', grades: 96 },
-    { id: 250, name: 'Andrea Mantegna', grades: 48 },
-    { id: 145, name: 'Gaia Borromini', grades: 74 },
-    { id: 196, name: 'Luigi Grimaldello', grades: 68 },
-    { id: 102, name: 'Piero della Francesca', grades: 50 },
-    { id: 120, name: 'Francesca da Polenta', grades: 84 }
-];
+// // Id  Name                Grades
+// // 213 Giuseppina della Rovere 78
+// // 110 Paola Cortellessa       96
+// // 250 Andrea Mantegna         48
+// // 145 Gaia Borromini          74
+// // 196 Luigi Grimaldello       68
+// // 102 Piero della Francesca   50
+// // 120 Francesca da Polenta    84
 
 
-//TODO Uppercase list
-const targhe = studenti.map(y => {
-    return { name: y.name.toUpperCase()};
-});
+// //! Creazione lista studenti
+// const studenti = [
+//     { id: 213, name: 'Giuseppina della Rovere', grades: 78 },
+//     { id: 110, name: 'Paola Cortellessa', grades: 96 },
+//     { id: 250, name: 'Andrea Mantegna', grades: 48 },
+//     { id: 145, name: 'Gaia Borromini', grades: 74 },
+//     { id: 196, name: 'Luigi Grimaldello', grades: 68 },
+//     { id: 102, name: 'Piero della Francesca', grades: 50 },
+//     { id: 120, name: 'Francesca da Polenta', grades: 84 }
+// ];
 
-console.log(targhe);
 
-//? Over 70
+// //TODO Uppercase list
+// const targhe = studenti.map(y => {
+//     return { name: y.name.toUpperCase()};
+// });
 
-goodGrades = studenti.filter(studente => studente.grades >= 70)
+// console.log(targhe);
 
-console.log(goodGrades)
+// //? Over 70
 
-idAndGrades = studenti.filter(studente => studente.grades >= 70 && studente.id >= 120)
+// goodGrades = studenti.filter(studente => studente.grades >= 70)
 
-console.log(idAndGrades)
+// console.log(goodGrades)
+
+// idAndGrades = studenti.filter(studente => studente.grades >= 70 && studente.id >= 120)
+
+// console.log(idAndGrades)
 
 
 // SNACK 3
@@ -93,7 +93,7 @@ console.log(idAndGrades)
 //     *   / Creare arrey di oggetti
 //     *   / Scegliere un modo per determinare la piu leggera e stamparla in console
 
-let biciclette = [
+const biciclette = [
     { nome: 'Bianchi Oltre XR4', peso: 7.5 },
     { nome: 'Cannondale SuperSix EVO', peso: 7.8 },
     { nome: 'Trek Emonda', peso: 7.2 },
@@ -102,4 +102,10 @@ let biciclette = [
   ];
   
   console.log(biciclette);
+  
+  const pesoMinimo = Infinity
+
+  biciclette.forEach({element}) => {
+    console.log({element});
+});
   
