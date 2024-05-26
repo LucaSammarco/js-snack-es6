e7a76763b96306bf8b9686b9305657ac6e241d32
Il tuo nome <la_tua_email@example.com>
2024-05-26 18:53:08 +0200

    added items array and randomletter function

--- js/script.js
+++ js/script.js
@@ -160,3 +160,27 @@ console.log(listaAggiornataSquadre);
 const nomeFalli = listaAggiornataSquadre.map(({ nome, falliSubiti }) => {
     return { nome, falliSubiti };
 });
+
+
+// BONUS 1: SNACK 5
+// Abbiamo questa lista di articoli di moda:
+// name        type      color
+// Poppy       tshirt    red
+// Jumping     occhiali  blue
+// CrissCross  scarpe    black
+// Jenny       borsa     pink
+// Questi articoli sono inseriti in un array di oggetti.
+// Dobbiamo attaccare a ognuno un cartellino position con una lettera dell'alfabeto generata casualmente e inserire gli oggetti così modificati in un nuovo array di oggetti.
+
+const items = [
+  { name: "Poppy", type: "tshirt", color: "red" },
+  { name: "Jumping", type: "occhiali", color: "blue" },
+  { name: "CrissCross", type: "scarpe", color: "black" },
+  { name: "Jenny", type: "borsa", color: "pink" }
+];
+
+
+function getRandomLetter() {
+  const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  return alphabet[Math.floor(Math.random() * alphabet.length)];
+}
